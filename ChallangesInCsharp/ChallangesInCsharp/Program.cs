4031a98f64486f70f07b3ef20a9e5db41fa7e18c
BakhtiyDeveloper <bakhtiyer.kurbanov@bk.ru>
2024-06-18 21:21:10 +0500

    update first essue

--- ChallangesInCsharp/ChallangesInCsharp/Program.cs
+++ ChallangesInCsharp/ChallangesInCsharp/Program.cs
@@ -1,2 +1,20 @@
 ﻿
-Console.WriteLine("Hello, World!");
+/* 
+A+B 
+It will be necessary to calculate the sum of integers A and B. 
+*/
+
+Console.WriteLine("Enter first number: ");
+string A = Console.ReadLine();
+int firstNumber = Convert.ToInt32(A);
+
+Console.WriteLine("Enter second number: ");
+string B = Console.ReadLine();
+int secondNumber = Convert.ToInt32(B);
+
+Console.WriteLine($"Result: {GetSumTwoNumbers(firstNumber, secondNumber)}");
+
+static int GetSumTwoNumbers(int firstNumber, int secondNumber)
+{
+    return firstNumber + secondNumber;
+}
