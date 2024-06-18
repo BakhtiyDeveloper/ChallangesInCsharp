b232f2c5155c32607f4aefcbba19c9c3f67a2099
BakhtiyDeveloper <bakhtiyer.kurbanov@bk.ru>
2024-06-18 23:02:42 +0500

    add 5th essue

--- ChallangesInCsharp/ChallangesInCsharp/Program.cs
+++ ChallangesInCsharp/ChallangesInCsharp/Program.cs
@@ -4,6 +4,7 @@ A+B
 It will be necessary to calculate the sum of integers A and B. 
 */
 
+/*
 Console.WriteLine("Enter first number: ");
 string A = Console.ReadLine();
 int firstNumber = Convert.ToInt32(A);
@@ -18,9 +19,63 @@ static int GetSumTwoNumbers(int firstNumber, int secondNumber)
 {
     return firstNumber + secondNumber;
 }
+*/
 
-Console.WriteLine("vfvfvfvf");
 
-Console.WriteLine("fvvfvvfv");
+/* 
+A rectangle: 
+You are given the sides a and b of a rectangle,  
+you need to find its area and perimeter and check the following condition. 
+*/
 
-Console.WriteLine("ddcdcdscdcd");
+/*
+Console.WriteLine("Enter first side of rectangle: "); 
+string userInputsideA = Console.ReadLine(); 
+int sideA = Convert.ToInt32(userInputsideA); 
+
+Console.WriteLine("Enter second side of rectangle: "); 
+string userInputsideB = Console.ReadLine(); 
+int sideB = Convert.ToInt32(userInputsideB); 
+ 
+Console.WriteLine($"Result : {GetParameterRectangle(sideA,sideB)}"); 
+ 
+static int GetParameterRectangle(int sideA,int sideB) 
+{ 
+    return sideA * sideB; 
+}
+*/
+
+/*
+ * The sides of a rectangle are given by a and b. Determine its surface.
+*/
+
+/*
+Console.WriteLine("Enter the first side of the rectangle: ");
+string userInputsurfaceA = Console.ReadLine();
+int surfaceA = Convert.ToInt32(userInputsurfaceA);
+
+Console.WriteLine("Enter the first side of the rectangle: ");
+string userInputsurfaceB = Console.ReadLine();
+int surfaceB = Convert.ToInt32(userInputsurfaceB);
+
+Console.WriteLine($"Result : {GetSurfaceRectangle(surfaceA, surfaceB)}");
+
+static int GetSurfaceRectangle(int surfaceA, int surfaceB)
+{
+    return 2 * (surfaceA + surfaceB);
+}
+
+*/
+
+Console.WriteLine("Enter diametr of cirkle:  ");
+string userInputDiametrCircle = Console.ReadLine();
+double diametrCirkle = Convert.ToDouble(userInputDiametrCircle);
+
+Console.WriteLine($"Result: {GetDiametrCircle(diametrCirkle)}");
+
+const double PI = 3.14; 
+static double GetDiametrCircle(double diametrCircle) 
+{
+    return double.Pi * diametrCircle;
+}
+
